59a490724e9014acacfcd1a5f9c5d1ff311cf535
Cengiz Poyraz <cengizp@hotmail.com>
2025-01-05 23:31:29 +0300

    run script parametrized

--- homeworks/hw2/a/run.sh
+++ homeworks/hw2/a/run.sh
@@ -1,1 +1,2 @@
-mpirun --allow-run-as-root -np 2 --use-hwthread-cpus --oversubscribe CMPE478-fall-2024-hw2-part-a 4
+#!/bin/bash
+mpirun --allow-run-as-root -np $1 --use-hwthread-cpus --oversubscribe CMPE478-fall-2024-hw2-part-a 4
